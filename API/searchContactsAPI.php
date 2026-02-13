b30ec8ab2d7c6af86269ec2659c59fe93ec8d0e6
NagelPoulose <nagel.poulose@gmail.com>
2026-02-12 23:35:34 -0500

    Updated searchContactsAPI to work properly and scan left to right

--- API/searchContactsAPI.php
+++ API/searchContactsAPI.php
@@ -16,8 +16,9 @@ if ($userId <= 0) {
 // 3) Connect to DB
 require_once __DIR__ . "/../db_connect.php";
 
-// 4) Search contacts for this user (FirstName, LastName, Phone, Email)
-$searchPattern = "%{$search}%";
+// 4) Search contacts - prefix match only (left-to-right, e.g. "jo" matches "John" but not "oh" or "n")
+$searchEscaped = str_replace(["%", "_"], ["\\%", "\\_"], $search);
+$searchPattern = $searchEscaped . "%";
 $stmt = $conn->prepare(
     "SELECT ID, FirstName, LastName, Phone, Email, DateCreated FROM Contacts 
      WHERE UserID = ? AND (FirstName LIKE ? OR LastName LIKE ? OR Phone LIKE ? OR Email LIKE ?)
